2e34a2dd088c591d8299b7d84936253fb212ca93
Luis Perez Contreras <luisperezcontreras@gmail.com>
2020-06-24 11:24:02 -0500

    Components CLI 1.- creamos dos componenetes , cabecera y listas las Agregamos a componente principal App.vue agregamos estilos de Bootstrap a index creamos group list de bootstrap en componente listas

--- cli/project-1/src/App.vue
+++ cli/project-1/src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class= "container">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Cabecera />
+    <listas />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Cabecera from './components/Cabecera.vue'
+import Listas from './components/Listas.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cabecera,
+    Listas
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
