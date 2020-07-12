2f6174ae3c672d997eedbce5f59b0e33732bc94a
Luis Perez Contreras <luisperezcontreras@gmail.com>
2020-07-12 13:13:52 -0500

    link-route Creamos link-route usando un for y creando ruta dinamicas atraves de un evento button

--- vueroute1/src/views/Fotos.vue
+++ vueroute1/src/views/Fotos.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <h1>Vista de Fotos</h1>
+            <router-link :to="{name:'fotos',params : {'id':item}}" 
+            v-for=" (item,index) of  fotosArreglo" :key="index">
+                <button> foto {{item}} </button>
+            </router-link>
         <fotografia />
     </div>
 </template>
@@ -10,6 +14,9 @@ import fotografia from "@/components/Fotografia.vue";
 export default {
     components: {
         fotografia
+    },
+    data(){
+        return { fotosArreglo : [1,2,3,4]}
     }
 }
 </script>
